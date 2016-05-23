2186b6eb2aa64bdc22372e90769a03544ffd92a8
windr <imrwz@icloud.com>
2016-05-23 11:12:32 +0800

    finished

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+
+
+static int list_init(void) {
+    struct task_struct *task, *p;
+    struct list_head *pos;
+    int count;
+    char *method;
+    count = 0;
+    p = NULL;
+    task = NULL;
+    pos = NULL;
+    method = NULL;
+    task = &init_task;
+    printk("list processes using for_each_process");
+    printk(KERN_ALERT"PID\tCOMM\n");
+    for_each_process(task) {
+
+        count++;
+
+        printk(KERN_ALERT "%d\t%s\n", task->pid, task->comm);
+
+    }
+
+    printk("current process count : %d\n", count);
+    printk(KERN_ALERT "this module is done by 冉惟之 stu. id : 2013060105023\n");
+
+    return 0;
+
+}
+
+static void list_exit(void) {
+
+    printk(KERN_ALERT "process list finished, now quiting\n");
+}
+
+module_init( list_init );
+
+module_exit( list_exit );
+
+MODULE_AUTHOR( "Along" );
+
+MODULE_LICENSE( "GPL" );
